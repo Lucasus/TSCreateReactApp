02e42441bd3283288c44bd4e2f30e97b6355aab7
Lucasus <lukasz.wiatrak@gmail.com>
2017-10-12 21:05:22 +0200

    Working proof of concept

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Component } from 'react';
+import './App.css';
+
+const logo = require('./logo.svg');
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo12563" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, effddddfdfdfdfdsfdfsdit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
